a650eeadc94b05e8c0a85724c3a099526a7f1682
thanle <thanh.le@sinnerschrader.com>
2017-06-10 10:39:42 +0200

    add filter

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,42 @@
-console.log("hello angular");
 var app = angular.module('app', ['controllers']);
 
 angular.module('controllers', [])
-  .controller('MainController', function ($scope) {
-    $scope.mydata = {
-      val: "jake",
-      istoolong: false
-    };
-    $scope.isTooLong = false;
-    $scope.$watch('mydata.val', function ($scope) {
-      if ($scope.length > 5) {
-        console.log("got here !" + $scope.isTooLong);
-        $scope.isTooLong = true;
+  .controller('MainController', function ($scope, $filter) {
+    $scope.shortcuts = {arr: [
+      {
+        keyCombi: "alt+tab",
+        description: "Switch between programs",
+        os: "Linux Window"
+      }, {
+        keyCombi: "jake",
+        description: 22,
+        os: "Linux Window"
+      }, {
+        keyCombi: "asdf",
+        description:11,
+        os: "Linux Window"
+      }, {
+        keyCombi: "basdf",
+        description:34,
+        os: "Linux Window"
       }
+    ]};
 
-    });
+  })
+  .filter('charlimit', function() {
+    return function(input, length) {
+      if(!length) {
+        length=10;
+      }
+      if(!input) {
+        return 'nothing here';
+      }
+      if(input.length <= length) {
+        return input;
+      } else {
+        return input.substring(0, length) + '...';
+      }
+    }
   })
   .controller('SubController', function ($scope) {
     $scope.classVar = 'orange';
